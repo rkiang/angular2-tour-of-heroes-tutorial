3c3a5280fc195f5668e0c9f6da5db7046daeee3d
rkiang <ryanlkiang@gmail.com>
2018-05-02 14:56:04 -0500

    used ngIf to display list, but not details if they are empty

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero: Hero;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -17,5 +18,7 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
